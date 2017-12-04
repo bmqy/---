af0f3f357a5f3c82f09dbf1a93db88a099fa4f9b
bmqy <qq88268459@126.com>
2017-12-04 15:22:18 +0800

    模板网
    模板网

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'zjwdb_6191634';
+$cfg_dbuser = 'zjwdb_6191634';
+$cfg_dbpwd = 'Ying1980';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
